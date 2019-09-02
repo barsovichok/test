406521ac27f180fe521a86ab069d179539eec847
barsovichok <taya.kulagina@gmail.com>
2019-09-02 15:06:37 +0500

    remove class from test_ochered.py

--- test_ochered.py
+++ test_ochered.py
@@ -1,58 +1,63 @@
 from ochered import query_queue
 from conftest import obj
-#district_code, district_queue
 
 
+def test_queue_select():
+    test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+    test_queue = test_queue[0]
+    test_queue = test_queue.split(' ')
+    assert test_queue[0].startswith("select") is True
 
-class TestPythonExamples:
-    def test_queue_select(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_queue = test_queue.split(' ')
-        assert test_queue[0].startswith("select") is True
 
-    def test_queue_some_param(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_queue = test_queue.split(' ')
-        assert test_queue[1] == 'some_param'
+def test_queue_some_param():
+    test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+    test_queue = test_queue[0]
+    test_queue = test_queue.split(' ')
+    assert test_queue[1] == 'some_param'
 
-    def test_queue_from(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_queue = test_queue.split(' ')
-        assert test_queue[2] == 'from'
 
-    def test_queue_db_name(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_queue = test_queue.split(' ')
-        assert test_queue[3] == 'Objects.DB'
+def test_queue_from():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     test_queue = test_queue[0]
+     test_queue = test_queue.split(' ')
+     assert test_queue[2] == 'from'
 
-    def test_queue_list(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        assert str(type(test_queue)) == "<class 'list'>"
 
-    def test_queue_find_str_isrelevant(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_str = test_queue.find("isrelevant = true")
-        assert test_str > -1
+def test_queue_db_name():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     test_queue = test_queue[0]
+     test_queue = test_queue.split(' ')
+     assert test_queue[3] == 'Objects.DB'
 
-    def test_queue_find_str_district(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_str = test_queue.find("district in")
-        assert test_str > -1
 
-    def test_queue_find_str_isbuildingliving(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_str = test_queue.find("isbuildingliving")
-        assert test_str > -1
+def test_queue_list():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     assert str(type(test_queue)) == "<class 'list'>"
 
-    def test_queue_count_str(self):
-        test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
-        test_queue = test_queue[0]
-        test_str = test_queue.count("and")
-        assert test_str == 2
+
+def test_queue_find_str_isrelevant():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     test_queue = test_queue[0]
+     test_str = test_queue.find("isrelevant = true")
+     assert test_str > -1
+
+
+def test_queue_find_str_district():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     test_queue = test_queue[0]
+     test_str = test_queue.find("district in")
+     assert test_str > -1
+
+
+def test_queue_find_str_isbuildingliving():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     test_queue = test_queue[0]
+     test_str = test_queue.find("isbuildingliving")
+     assert test_str > -1
+
+
+def test_queue_count_str():
+     test_queue = query_queue(select_params=1, table_name="Objects.DB", district_code=['SAO-mkad', 'SVAO-mkad', 'VAO-mkad', 'UVAO-mkad', 'UAO-mkad', 'UZAO-mkad', 'ZAO-mkad', 'SZAO-mkad'], obj=obj)
+     test_queue = test_queue[0]
+     test_str = test_queue.count("and")
+     assert test_str == 2
